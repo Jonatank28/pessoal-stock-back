4cd3e2b8767644a8c5501d9eb6028fdc23e4efb2
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-08-03 23:15:44 -0300

    tipos e tags enviados do anco para o front

--- src/controllers/dataInitial.controller.ts
+++ src/controllers/dataInitial.controller.ts
@@ -27,17 +27,24 @@ export class DataInitialController {
                 (SELECT c.name FROM tag AS c  WHERE c.tagID = a.tagID) as tagID,
                 (SELECT b.name FROM type AS b WHERE b.typeID = a.typeID) as typeID  
             FROM 
-            transaction AS a;
+            transaction AS a
+            ORDER BY 1 DESC;
             `
             const resultsqlGetTransaction = await this.db.query(
                 sqlGetTransaction
             )
+            const sqlGetTypes = 'SELECT typeID AS id, name FROM type'
+            const resultSqlGetTypes = await this.db.query(sqlGetTypes)
+            const sqlGetTags = 'SELECT tagID AS id, name FROM tag'
+            const resultSqlGetTags = await this.db.query(sqlGetTags)
 
             const result = {
-                transactions: resultsqlGetTransaction,
                 balanceGlobal: resultSqlGet[0],
+                transactions: resultsqlGetTransaction,
+                tags: resultSqlGetTags,
+                types: resultSqlGetTypes,
             }
-            console.log('🚀 ~ result:', result)
+
             res.status(201).json(result)
         } catch (error) {
             console.log(error)
