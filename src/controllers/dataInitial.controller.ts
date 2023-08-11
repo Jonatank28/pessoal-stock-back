6ba7dce4c6c61d910ac55037549ca30e4b1534f8
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-08-11 00:14:32 -0300

    aaa

--- src/controllers/dataInitial.controller.ts
+++ src/controllers/dataInitial.controller.ts
@@ -20,6 +20,41 @@ export class DataInitialController {
             LIMIT 1;
             `
             const resultSqlGet = await this.db.query(sqlGet, [userID])
+            const sqlGroupMonth = `
+            SELECT 
+                CASE 
+                WHEN DATE(a.updateDate) = CURDATE() THEN 'Hoje'
+                WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 'Ontem'
+                ELSE CONCAT(
+                    CASE DAYOFWEEK(a.updateDate)
+                        WHEN 1 THEN 'Domingo'
+                        WHEN 2 THEN 'Segunda-feira'
+                        WHEN 3 THEN 'Terça-feira'
+                        WHEN 4 THEN 'Quarta-feira'
+                        WHEN 5 THEN 'Quinta-feira'
+                        WHEN 6 THEN 'Sexta-feira'
+                        WHEN 7 THEN 'Sábado'
+                    END,
+                    ', ',
+                    DATE_FORMAT(a.updateDate, '%d')
+                    )
+                END as day_date
+            FROM  transaction AS a
+            WHERE userID = ?
+            GROUP BY
+                CASE
+                    WHEN DATE(a.updateDate) = CURDATE() THEN 1
+                    WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 2
+                    ELSE 3
+                END, a.updateDate DESC
+            ORDER BY
+                CASE
+                    WHEN DATE(a.updateDate) = CURDATE() THEN 1
+                    WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 2
+                    ELSE 3
+                END, a.updateDate DESC;
+            `
+
             const sqlGetTransaction = `
             SELECT 
                 a.transactionID,
@@ -53,10 +88,42 @@ export class DataInitialController {
                     ELSE 3
                 END, a.updateDate DESC;
             `
+
+            const resultSqlGroupMonth = await this.db.query(sqlGroupMonth, [
+                userID,
+            ])
             const resultsqlGetTransaction = await this.db.query(
                 sqlGetTransaction,
                 [userID]
             )
+
+            // Criar um array onde cada posição é um array vazio
+            const transactionsByMonth = new Array(
+                resultSqlGroupMonth.length
+            ).fill([])
+
+            // Preencher o array com as transações agrupadas por mês
+            resultSqlGroupMonth.forEach(({ day_date }, index) => {
+                transactionsByMonth[index] = {
+                    day: day_date,
+                    transactions: [],
+                }
+            })
+
+            resultsqlGetTransaction.forEach((transaction) => {
+                const { day_date } = transaction
+
+                // Encontrar o índice correspondente ao mês e adicionar a transação
+                const monthIndex = resultSqlGroupMonth.findIndex(
+                    (item) => item.day_date === day_date
+                )
+                if (monthIndex !== -1) {
+                    transactionsByMonth[monthIndex].transactions.push(
+                        transaction
+                    )
+                }
+            })
+
             const sqlGetTypes = 'SELECT typeID AS id, name FROM type'
             const resultSqlGetTypes = await this.db.query(sqlGetTypes)
             const sqlGetTags = 'SELECT tagID AS id, name FROM tag'
@@ -64,7 +131,7 @@ export class DataInitialController {
 
             const result = {
                 balanceGlobal: resultSqlGet[0],
-                transactions: resultsqlGetTransaction,
+                transactions: transactionsByMonth,
                 tags: resultSqlGetTags,
                 types: resultSqlGetTypes,
             }
