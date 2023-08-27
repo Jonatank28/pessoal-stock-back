e2f19f22b60c1385b8d42426165ad4c84eb7e8e8
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-08-26 22:46:11 -0300

    novo grafico

--- src/controllers/dataInitial.controller.ts
+++ src/controllers/dataInitial.controller.ts
@@ -10,49 +10,88 @@ export class DataInitialController {
     async DataInitial(@Body() data, @Res() res: Response) {
         try {
             const userID = data.data.userID
+            const month = data.data.month_number
+            const year = data.data.year
+
+            const getMonthTransactions = `
+            SELECT 
+                DATE_FORMAT(updateDate, '%Y') AS year,
+                DATE_FORMAT(updateDate, '%m') AS month_number,
+                CASE DATE_FORMAT(updateDate, '%m')
+                    WHEN '01' THEN 'Janeiro'
+                    WHEN '02' THEN 'Fevereiro'
+                    WHEN '03' THEN 'Março'
+                    WHEN '04' THEN 'Abril'
+                    WHEN '05' THEN 'Maio'
+                    WHEN '06' THEN 'Junho'
+                    WHEN '07' THEN 'Julho'
+                    WHEN '08' THEN 'Agosto'
+                    WHEN '09' THEN 'Setembro'
+                    WHEN '10' THEN 'Outubro'
+                    WHEN '11' THEN 'Novembro'
+                    WHEN '12' THEN 'Dezembro'
+                END AS month_name
+            FROM transaction
+            WHERE userID = ?
+                AND typeID IN (1, 2)
+            GROUP BY year, month_number, month_name
+            ORDER BY year, month_number;
+
+             `
+            const resultGetMonthTransactions = await this.db.query(
+                getMonthTransactions,
+                [userID]
+            )
+
+            // Parametros padrão
+            const parametersDefault = [
+                userID,
+                year ?? resultGetMonthTransactions[0].year,
+                month ?? resultGetMonthTransactions[0].month_number,
+            ]
+
             const sqlGet = `
             SELECT
-                ROUND(COALESCE((SELECT SUM(b.value) FROM transaction AS b WHERE b.typeID = 2 AND b.userID = a.userID), 0), 2) AS revenue,
-                ROUND(COALESCE((SELECT SUM(c.value) FROM transaction AS c WHERE c.typeID = 1 AND c.userID = a.userID), 0), 2) AS expense,
-                ROUND(COALESCE((SELECT SUM(b.value) FROM transaction AS b WHERE b.typeID = 2 AND b.userID = a.userID), 0) - COALESCE((SELECT SUM(c.value) FROM transaction AS c WHERE c.typeID = 1 AND c.userID = a.userID), 0), 2) AS currentBalance
-            FROM transaction AS a
-            WHERE a.userID = ?
-            LIMIT 1;
+                ROUND(SUM(CASE WHEN t.typeID = 2 THEN t.value ELSE 0 END), 2) AS revenue,
+                ROUND(SUM(CASE WHEN t.typeID = 1 THEN t.value ELSE 0 END), 2) AS expense,
+                ROUND(SUM(CASE WHEN t.typeID = 2 THEN t.value ELSE 0 END) - SUM(CASE WHEN t.typeID = 1 THEN t.value ELSE 0 END), 2) AS currentBalance
+            FROM transaction AS t
+            WHERE t.userID = ?
+            AND YEAR(t.updateDate) = ?
+            AND MONTH(t.updateDate) = ?
+            LIMIT 1;;
             `
-            const resultSqlGet = await this.db.query(sqlGet, [userID])
+            const resultSqlGet = await this.db.query(sqlGet, parametersDefault)
+
             const sqlGroupMonth = `
             SELECT 
                 CASE 
-                WHEN DATE(a.updateDate) = CURDATE() THEN 'Hoje'
-                WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 'Ontem'
-                ELSE CONCAT(
-                    CASE DAYOFWEEK(a.updateDate)
-                        WHEN 1 THEN 'Domingo'
-                        WHEN 2 THEN 'Segunda-feira'
-                        WHEN 3 THEN 'Terça-feira'
-                        WHEN 4 THEN 'Quarta-feira'
-                        WHEN 5 THEN 'Quinta-feira'
-                        WHEN 6 THEN 'Sexta-feira'
-                        WHEN 7 THEN 'Sábado'
-                    END,
-                    ', ',
-                    DATE_FORMAT(a.updateDate, '%d')
+                    WHEN DATE(a.updateDate) = CURDATE() THEN 'Hoje'
+                    WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 'Ontem'
+                    ELSE CONCAT(
+                        CASE DAYOFWEEK(a.updateDate)
+                            WHEN 1 THEN 'Domingo'
+                            WHEN 2 THEN 'Segunda-feira'
+                            WHEN 3 THEN 'Terça-feira'
+                            WHEN 4 THEN 'Quarta-feira'
+                            WHEN 5 THEN 'Quinta-feira'
+                            WHEN 6 THEN 'Sexta-feira'
+                            WHEN 7 THEN 'Sábado'
+                        END,
+                        ', ',
+                        DATE_FORMAT(a.updateDate, '%d')
                     )
-                END as day_date
-            FROM  transaction AS a
-            WHERE userID = ?
-            GROUP BY
-                CASE
-                    WHEN DATE(a.updateDate) = CURDATE() THEN 1
-                    WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 2
-                    ELSE 3
-                END, a.updateDate DESC
-            ORDER BY
-                CASE
-                    WHEN DATE(a.updateDate) = CURDATE() THEN 1
-                    WHEN DATE(a.updateDate) = DATE_SUB(CURDATE(), INTERVAL 1 DAY) THEN 2
-                    ELSE 3
-                END, a.updateDate DESC;
+                END as day_date,
+                SUM(CASE WHEN a.typeID = 1 THEN a.value ELSE 0 END) as revenue,
+                SUM(CASE WHEN a.typeID = 2 THEN a.value ELSE 0 END) as expense,
+                SUM(CASE WHEN a.typeID = 2 THEN a.value ELSE 0 END) - SUM(CASE WHEN a.typeID = 1 THEN a.value ELSE 0 END) as net_amount
+            FROM transaction AS a
+                WHERE userID = ?
+                AND typeID IN (1, 2)
+                AND YEAR(a.updateDate) = ?  
+                AND MONTH(a.updateDate) = ?
+                GROUP BY day_date
+                ORDER BY a.updateDate DESC
             `
 
             const sqlGetTransaction = `
@@ -81,6 +120,9 @@ export class DataInitialController {
                 (SELECT b.name FROM type AS b WHERE b.typeID = a.typeID) as typeID  
             FROM  transaction AS a
             WHERE userID = ?
+            AND typeID IN (1, 2)
+            AND YEAR(a.updateDate) = ?  
+            AND MONTH(a.updateDate) = ?
             ORDER BY
                 CASE
                     WHEN DATE(a.updateDate) = CURDATE() THEN 1
@@ -89,31 +131,33 @@ export class DataInitialController {
                 END, a.updateDate DESC;
             `
 
-            const resultSqlGroupMonth = await this.db.query(sqlGroupMonth, [
-                userID,
-            ])
+            const resultSqlGroupMonth = await this.db.query(
+                sqlGroupMonth,
+                parametersDefault
+            )
             const resultsqlGetTransaction = await this.db.query(
                 sqlGetTransaction,
-                [userID]
+                parametersDefault
             )
 
-            // Criar um array onde cada posição é um array vazio
             const transactionsByMonth = new Array(
                 resultSqlGroupMonth.length
             ).fill([])
 
-            // Preencher o array com as transações agrupadas por mês
-            resultSqlGroupMonth.forEach(({ day_date }, index) => {
-                transactionsByMonth[index] = {
-                    day: day_date,
-                    transactions: [],
+            resultSqlGroupMonth.forEach(
+                ({ day_date, revenue, expense, net_amount }, index) => {
+                    transactionsByMonth[index] = {
+                        day: day_date,
+                        revenue,
+                        expense,
+                        net_amount,
+                        transactions: [],
+                    }
                 }
-            })
+            )
 
             resultsqlGetTransaction.forEach((transaction) => {
                 const { day_date } = transaction
-
-                // Encontrar o índice correspondente ao mês e adicionar a transação
                 const monthIndex = resultSqlGroupMonth.findIndex(
                     (item) => item.day_date === day_date
                 )
@@ -134,6 +178,7 @@ export class DataInitialController {
                 transactionsGroup: transactionsByMonth,
                 tags: resultSqlGetTags,
                 types: resultSqlGetTypes,
+                months: resultGetMonthTransactions,
             }
 
             res.status(201).json(result)
